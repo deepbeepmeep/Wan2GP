d1ba9a23c4d1b4ab024f0e2e2d66eafa5123b2f8
Christopher Anderson <sfinktah@github.spamtrak.org>
2025-06-14 16:19:46 +1000

    Fixed: if ... not any("fp16","FP16") in raw_filename...

--- wgp.py
+++ wgp.py
@@ -1746,7 +1746,8 @@ def get_model_filename(model_type, quantization ="int8", dtype_policy = ""):
         else:
             raw_filename = choices[0]
 
-    if dtype == torch.float16 and not any("fp16","FP16") in raw_filename and model_family == "wan" and finetune_def == None :
+    if (dtype == torch.float16 and not any(s in raw_filename for s in ("fp16", "FP16")) and model_family == "wan"
+            and finetune_def is None):
         if "quanto_int8" in raw_filename:
             raw_filename = raw_filename.replace("quanto_int8", "quanto_fp16_int8")
         elif "quanto_bf16_int8" in raw_filename:
